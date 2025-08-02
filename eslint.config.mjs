d18d901bd0f9029c707b2969c0d47339b40eb28c
Abraham <abrahambillclinton@.com>
2025-08-02 18:12:41 +0530

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/no-unescaped-entities": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
